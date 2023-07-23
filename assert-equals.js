217cf9d62a3f2d8ffe3e46e2a75f6ebfd04d8866
sgroil <laurencesgroi@hotmail.co.uk>
2023-07-23 11:35:30 +0100

    initial commit

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,55 @@
-function assertEquals(expect, actual) {}
+function assertEquals(expect, actual) {
+  const expectedType = typeof expect;
+  const actualType = typeof actual;
 
-module.exports = assertEquals
+  if (expectedType !== actualType) {
+    throw new Error(
+      `Expected type ${expectedType}, but found type ${actualType}`
+    );
+  } else if (Array.isArray(expect) && Array.isArray(actual)) {
+    if (expect.length !== actual.length) {
+      throw new Error(
+        `Expected array length ${expect.length}, but found ${actual.length}`
+      );
+    }
+
+    for (let i = 0; i < expect.length; i++) {
+      if (expect[i] !== actual[i]) {
+        throw new Error(`Expected "${expect[i]}" but found "${actual[i]}"`);
+      }
+    }
+  } else if (expectedType === "object" && expect !== null && actual !== null) {
+    for (const key in expect) {
+      if (!(key in actual)) {
+        throw new Error(
+          `Expected object with key "${key}" but found object without this key`
+        );
+      }
+
+      if (expect[key] !== actual[key]) {
+        throw new Error(
+          `Expected same values, but found different values for key [${key}]`
+        );
+      }
+    }
+
+    for (const key in actual) {
+      if (!(key in expect)) {
+        throw new Error(`Found unexpected key [${key}] in object`);
+      }
+    }
+  } else if (expectedType === "number") {
+    if (expect !== actual) {
+      throw new Error(`Expected ${expect} but found ${actual}`);
+    }
+  } else if (expect !== actual) {
+    throw new Error(`Expected "${expect}" but found "${actual}"`);
+  }
+}
+
+//assertEquals(
+// { name: "laurie", age: { now: 31 } },
+//  { name: "laurie", age: { now: 31 } }
+//);
+
+module.exports = assertEquals;
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,13 +1,80 @@
-const assertEquals = require('./assert-equals')
+const assertEquals = require("./assert-equals");
 
-describe('assertEquals', () => {
-  describe('when expected and actual are the same string', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals('abc', 'abc')).not.toThrow()
-    })
-  })
+describe("assertEquals", () => {
+  describe("when expected and actual are the same string", () => {
+    it("returns without throwing an error", () => {
+      expect(() => assertEquals("abc", "abc")).not.toThrow();
+    });
+  });
 
-  describe('when expected and actual are different strings', () => {
-    it.todo('throws an error')
-  })
-})
+  describe("when expected and actual are different strings", () => {
+    it("throws an error", () => {
+      expect(() => assertEquals("abc", "abcd")).toThrow(
+        `Expected "abc" but found "abcd"`
+      );
+    });
+  });
+
+  describe("when expected and actual are different numbers", () => {
+    it("throws an error", () => {
+      const expectedMessage = `Expected 1 but found 2`;
+      expect(() => assertEquals(1, 2)).toThrow(expectedMessage);
+    });
+  });
+
+  describe("when expected and actual are different types", () => {
+    it("throws an error", () => {
+      expect(() => assertEquals(1, "1")).toThrow(
+        `Expected type number, but found type string`
+      );
+    });
+  });
+
+  describe("when expected and actual are different lengths", () => {
+    it("throws an error", () => {
+      expect(() => assertEquals(["a", "b"], ["a", "b", "c"])).toThrow(
+        `Expected array length 2, but found 3`
+      );
+    });
+  });
+
+  describe("when expected and actual are different arrays of the same length", () => {
+    it("throws an error", () => {
+      expect(() => assertEquals(["a", "b"], ["a", "d"])).toThrow(
+        `Expected "b" but found "d"`
+      );
+    });
+  });
+
+  describe("when expected and actual are objects with different number of keys ", () => {
+    it("throws an error", () => {
+      expect(() =>
+        assertEquals(
+          { name: "laurie", age: 31 },
+          { name: "laurie", age: 31, height: 182 }
+        )
+      ).toThrow(`Found unexpected key [height] in object`);
+    });
+  });
+
+  describe("when expected and actual are objects with different values", () => {
+    it("throws an error", () => {
+      expect(() =>
+        assertEquals({ name: "laurie", age: 31 }, { name: "laurie", age: 30 })
+      ).toThrow(
+        `Expected same values, but found different values for key [age]`
+      );
+    });
+  });
+
+  describe("when expected and actual are nested objects", () => {
+    it("returns without throwing an error", () => {
+      expect(() =>
+        assertEquals(
+          { name: "laurie", age: { now: 31 } },
+          { name: "laurie", age: { now: 31 } }
+        )
+      ).not.toThrow();
+    });
+  });
+});
