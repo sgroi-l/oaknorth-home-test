69ca8556850a0b44ce3c6ac5a8d0bc351f123897
sgroil <laurencesgroi@hotmail.co.uk>
2023-07-24 12:35:40 +0100

    added comments

--- assert-equals.js
+++ assert-equals.js
@@ -1,18 +1,21 @@
 function assertEquals(expect, actual) {
+  // Check if at least two arguments (expect and actual) are provided
   if (arguments.length < 2) {
     throw new Error("Expect at least two arguments: expect and actual");
   }
 
+  // Determine the types of expected and actual values
   const expectedType = Array.isArray(expect) ? "array" : typeof expect;
   const actualType = Array.isArray(actual) ? "array" : typeof actual;
 
+  // Check if the types of expected and actual values match
   if (expectedType !== actualType) {
-    // throws an error if types are different
+    // Throws an error if types are different
     throw new Error(
       `Expected type ${expectedType}, but found type ${actualType}`
     );
   }
-
+  // Perform comparison based on the data types
   switch (expectedType) {
     case "array":
       compareArrays(expect, actual);
@@ -28,12 +31,14 @@ function assertEquals(expect, actual) {
 }
 
 function compareArrays(expect, actual) {
+  // Check if the array lengths are different
   if (expect.length !== actual.length) {
     throw new Error(
       `Expected array length ${expect.length}, but found ${actual.length}`
     );
   }
 
+  // Iterate through the array elements and compare them
   for (let i = 0; i < expect.length; i++) {
     if (expect[i] !== actual[i]) {
       throw new Error(`Expected "${expect[i]}" but found "${actual[i]}"`);
@@ -42,16 +47,20 @@ function compareArrays(expect, actual) {
 }
 
 function compareObjects(expect, actual) {
+  // Check if both objects are not null
   if (expect !== null && actual !== null) {
+    // Get the keys of expected and actual objects
     const expectedKeys = Object.keys(expect);
     const actualKeys = Object.keys(actual);
 
+    // Check if the number of properties (keys) is the same
     if (expectedKeys.length !== actualKeys.length) {
       throw new Error(
         `Expected keys [${expectedKeys}], but found keys [${actualKeys}]`
       );
     }
 
+    // Iterate through the object properties
     for (const key of expectedKeys) {
       if (!(key in actual)) {
         throw new Error(
@@ -71,6 +80,7 @@ function compareObjects(expect, actual) {
 }
 
 function compareOthers(expect, actual) {
+  // If the data types are not object or array, perform a direct comparison
   if (expect !== actual) {
     throw new Error(
       `Expected ${JSON.stringify(expect)} but found ${JSON.stringify(actual)}`
